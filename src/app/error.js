651767810b1edcf418f083af5487e5ca78c2ef12
Abu Rahian <aburaihan363@gmail.com>
2025-11-26 02:09:12 +0600

    add error page

---
+++ src/app/error.js
@@ -0,0 +1,8 @@
+export default function errorPage() {
+  return (
+    <div className="text-center my-14">
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you are looking for doesn’t exist.</p>
+    </div>
+  );
+}
